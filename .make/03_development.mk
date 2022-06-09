e9936b937de2b80ab0be30247c19581a5cb20205
Calvin Alkan <calvin@snicco.io>
2022-06-09 20:07:07 +0200

    chore(monorepo): commit locally

--- .make/03_development.mk
+++ .make/03_development.mk
@@ -1,9 +1,16 @@
 ##@ [Development]
 
-.PHONY:npm
+.PHONY:npm commit _clean_working_dir
 
-npm: ## Run an npm script. Usage: make npm ARGS="run dev"
+npm: ## Run an npm script. Usage: make npm ARGS="run dev".
 	$(MAYBE_RUN_NODE_IN_DOCKER) npm ${ARGS}
 
-commit: ## Launch the interactive commit tool
-	$(MAYBE_RUN_NODE_IN_DOCKER) npm run commit
+commit:  ## Launch the interactive commit tool (node required locally).
+	@if ! command -v npm &> /dev/null; \
+    then \
+        printf "This make target can currently not run in docker and requires node + npm on your local machine.\n";\
+        exit 1;\
+    fi
+	npm run commit;
+
+
