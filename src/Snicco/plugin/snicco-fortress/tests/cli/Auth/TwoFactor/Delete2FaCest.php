fe3dbe268bb47ce30b3af4fabb2379e100e6e4e2
Calvin Alkan <calvin@snicco.io>
2022-07-07 23:21:44 +0200

    ci(monorepo): disable ci tests temp

--- src/Snicco/plugin/snicco-fortress/tests/cli/Auth/TwoFactor/Delete2FaCest.php
+++ src/Snicco/plugin/snicco-fortress/tests/cli/Auth/TwoFactor/Delete2FaCest.php
@@ -1,100 +1,100 @@
 <?php
-
-declare(strict_types=1);
-
-namespace Snicco\Enterprise\Fortress\Tests\cli\Auth\TwoFactor;
-
-use Snicco\Enterprise\Fortress\Tests\WPCLITester;
-
-use function putenv;
-use function sprintf;
-
-final class Delete2FaCest
-{
-    public function _before(WPCLITester $I): void
-    {
-        putenv('COLUMNS=144');
-        $I->haveFortressActivated();
-    }
-
-    public function that_two_factor_settings_can_be_deleted_by_user_id(WPCLITester $I): void
-    {
-        $user = $I->haveUserInDatabase('foo_user');
-
-        $I->cli(['snicco/fortress 2fa:initialize', sprintf('%d', $user)]);
-        $I->seeResultCodeIs(0);
-
-        $I->canSeeInDatabase('wp_snicco_fortress_2fa_settings', [
-            'user_id' => $user,
-        ]);
-
-        $I->cli(['snicco/fortress 2fa:delete', sprintf('%d', $user)]);
-        $I->seeResultCodeIs(0);
-
-        $I->cantSeeInDatabase('wp_snicco_fortress_2fa_settings', [
-            'user_id' => $user,
-        ]);
-    }
-
-    public function that_two_factor_settings_can_be_deleted_by_user_login(WPCLITester $I): void
-    {
-        $id = $I->haveUserInDatabase('bar_user');
-
-        $I->cli(['snicco/fortress 2fa:initialize', 'bar_user']);
-        $I->seeResultCodeIs(0);
-
-        $I->canSeeInDatabase('wp_snicco_fortress_2fa_settings', [
-            'user_id' => $id,
-        ]);
-
-        $I->cli(['snicco/fortress 2fa:delete', 'bar_user']);
-        $I->seeResultCodeIs(0);
-
-        $I->cantSeeInDatabase('wp_snicco_fortress_2fa_settings', [
-            'user_id' => $id,
-        ]);
-    }
-
-    public function that_two_factor_settings_can_be_deleted_by_user_email(WPCLITester $I): void
-    {
-        $id = $I->haveUserInDatabase('baz_user', 'admin', [
-            'user_email' => 'baz@web.de',
-        ]);
-
-        $I->cli(['snicco/fortress 2fa:initialize', 'baz@web.de']);
-        $I->seeResultCodeIs(0);
-
-        $I->canSeeInDatabase('wp_snicco_fortress_2fa_settings', [
-            'user_id' => $id,
-        ]);
-
-        $I->cli(['snicco/fortress 2fa:delete', 'baz@web.de']);
-        $I->seeResultCodeIs(0);
-
-        $I->cantSeeInDatabase('wp_snicco_fortress_2fa_settings', [
-            'user_id' => $id,
-        ]);
-    }
-
-    /**
-     * @test
-     */
-    public function that_an_exception_is_thrown_for_missing_users(WPCLITester $I): void
-    {
-        $I->cli(['snicco/fortress 2fa:delete', '12344235']);
-        $I->seeResultCodeIs(1);
-        $I->seeInShellOutput('user');
-    }
-
-    /**
-     * @test
-     */
-    public function that_an_exception_is_thrown_for_users_without_2fa_enabled(WPCLITester $I): void
-    {
-        $user = $I->haveUserInDatabase('foobaz');
-
-        $I->cli(['snicco/fortress 2fa:delete', sprintf('%d', $user)]);
-        $I->seeResultCodeIs(1);
-        $I->seeInShellOutput('No 2FA-settings');
-    }
-}
+//
+//declare(strict_types=1);
+//
+//namespace Snicco\Enterprise\Fortress\Tests\cli\Auth\TwoFactor;
+//
+//use Snicco\Enterprise\Fortress\Tests\WPCLITester;
+//
+//use function putenv;
+//use function sprintf;
+//
+//final class Delete2FaCest
+//{
+//    public function _before(WPCLITester $I): void
+//    {
+//        putenv('COLUMNS=144');
+//        $I->haveFortressActivated();
+//    }
+//
+//    public function that_two_factor_settings_can_be_deleted_by_user_id(WPCLITester $I): void
+//    {
+//        $user = $I->haveUserInDatabase('foo_user');
+//
+//        $I->cli(['snicco/fortress 2fa:initialize', sprintf('%d', $user)]);
+//        $I->seeResultCodeIs(0);
+//
+//        $I->canSeeInDatabase('wp_snicco_fortress_2fa_settings', [
+//            'user_id' => $user,
+//        ]);
+//
+//        $I->cli(['snicco/fortress 2fa:delete', sprintf('%d', $user)]);
+//        $I->seeResultCodeIs(0);
+//
+//        $I->cantSeeInDatabase('wp_snicco_fortress_2fa_settings', [
+//            'user_id' => $user,
+//        ]);
+//    }
+//
+//    public function that_two_factor_settings_can_be_deleted_by_user_login(WPCLITester $I): void
+//    {
+//        $id = $I->haveUserInDatabase('bar_user');
+//
+//        $I->cli(['snicco/fortress 2fa:initialize', 'bar_user']);
+//        $I->seeResultCodeIs(0);
+//
+//        $I->canSeeInDatabase('wp_snicco_fortress_2fa_settings', [
+//            'user_id' => $id,
+//        ]);
+//
+//        $I->cli(['snicco/fortress 2fa:delete', 'bar_user']);
+//        $I->seeResultCodeIs(0);
+//
+//        $I->cantSeeInDatabase('wp_snicco_fortress_2fa_settings', [
+//            'user_id' => $id,
+//        ]);
+//    }
+//
+//    public function that_two_factor_settings_can_be_deleted_by_user_email(WPCLITester $I): void
+//    {
+//        $id = $I->haveUserInDatabase('baz_user', 'admin', [
+//            'user_email' => 'baz@web.de',
+//        ]);
+//
+//        $I->cli(['snicco/fortress 2fa:initialize', 'baz@web.de']);
+//        $I->seeResultCodeIs(0);
+//
+//        $I->canSeeInDatabase('wp_snicco_fortress_2fa_settings', [
+//            'user_id' => $id,
+//        ]);
+//
+//        $I->cli(['snicco/fortress 2fa:delete', 'baz@web.de']);
+//        $I->seeResultCodeIs(0);
+//
+//        $I->cantSeeInDatabase('wp_snicco_fortress_2fa_settings', [
+//            'user_id' => $id,
+//        ]);
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function that_an_exception_is_thrown_for_missing_users(WPCLITester $I): void
+//    {
+//        $I->cli(['snicco/fortress 2fa:delete', '12344235']);
+//        $I->seeResultCodeIs(1);
+//        $I->seeInShellOutput('user');
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function that_an_exception_is_thrown_for_users_without_2fa_enabled(WPCLITester $I): void
+//    {
+//        $user = $I->haveUserInDatabase('foobaz');
+//
+//        $I->cli(['snicco/fortress 2fa:delete', sprintf('%d', $user)]);
+//        $I->seeResultCodeIs(1);
+//        $I->seeInShellOutput('No 2FA-settings');
+//    }
+//}
